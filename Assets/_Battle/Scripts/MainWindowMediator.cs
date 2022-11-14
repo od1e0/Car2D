1f0146b14abe23baa7247284d483b975c3ba46fd
od1e0 <88845348+od1e0@users.noreply.github.com>
2022-11-14 22:20:20 +0300

    Dz 5/6

--- Assets/_Battle/Scripts/MainWindowMediator.cs
+++ Assets/_Battle/Scripts/MainWindowMediator.cs
@@ -11,6 +11,7 @@ namespace BattleScripts
         [SerializeField] private TMP_Text _countMoneyText;
         [SerializeField] private TMP_Text _countHealthText;
         [SerializeField] private TMP_Text _countPowerText;
+        [SerializeField] private TMP_Text _countCrimeText;
 
         [Header("Enemy Stats")]
         [SerializeField] private TMP_Text _countPowerEnemyText;
@@ -27,12 +28,18 @@ namespace BattleScripts
         [SerializeField] private Button _addPowerButton;
         [SerializeField] private Button _minusPowerButton;
 
+        [Header("Crime Buttons")]
+        [SerializeField] private Button _addCrimeButton;
+        [SerializeField] private Button _minusCrimeButton;
+
         [Header("Other Buttons")]
         [SerializeField] private Button _fightButton;
+        [SerializeField] private Button _escapeButton;
 
         private PlayerData _money;
         private PlayerData _heath;
         private PlayerData _power;
+        private PlayerData _crime;
 
         private Enemy _enemy;
 
@@ -44,6 +51,7 @@ namespace BattleScripts
             _money = CreatePlayerData(DataType.Money);
             _heath = CreatePlayerData(DataType.Health);
             _power = CreatePlayerData(DataType.Power);
+            _crime = CreatePlayerData(DataType.Crime);
 
             Subscribe();
         }
@@ -53,6 +61,7 @@ namespace BattleScripts
             DisposePlayerData(ref _money);
             DisposePlayerData(ref _heath);
             DisposePlayerData(ref _power);
+            DisposePlayerData(ref _crime);
 
             Unsubscribe();
         }
@@ -84,7 +93,11 @@ namespace BattleScripts
             _addPowerButton.onClick.AddListener(IncreasePower);
             _minusPowerButton.onClick.AddListener(DecreasePower);
 
+            _addCrimeButton.onClick.AddListener(IncreaseCrime);
+            _minusCrimeButton.onClick.AddListener(DecreaseCrime);
+
             _fightButton.onClick.AddListener(Fight);
+            _escapeButton.onClick.AddListener(Escape);
         }
 
         private void Unsubscribe()
@@ -98,7 +111,11 @@ namespace BattleScripts
             _addPowerButton.onClick.RemoveAllListeners();
             _minusPowerButton.onClick.RemoveAllListeners();
 
+            _addCrimeButton.onClick.RemoveAllListeners();
+            _minusCrimeButton.onClick.RemoveAllListeners();
+
             _fightButton.onClick.RemoveAllListeners();
+            _escapeButton.onClick.RemoveAllListeners();
         }
 
 
@@ -111,6 +128,9 @@ namespace BattleScripts
         private void IncreasePower() => IncreaseValue(_power);
         private void DecreasePower() => DecreaseValue(_power);
 
+        private void IncreaseCrime() => IncreaseValue(_crime);
+        private void DecreaseCrime() => DecreaseValue(_crime);
+
         private void IncreaseValue(PlayerData playerData) => AddToValue(1, playerData);
         private void DecreaseValue(PlayerData playerData) => AddToValue(-1, playerData);
 
@@ -118,6 +138,7 @@ namespace BattleScripts
         {
             playerData.Value += addition;
             ChangeDataWindow(playerData);
+            UpdateEscapeButtonVisibility();
         }
 
 
@@ -138,9 +159,25 @@ namespace BattleScripts
                 DataType.Money => _countMoneyText,
                 DataType.Health => _countHealthText,
                 DataType.Power => _countPowerText,
+                DataType.Crime => _countCrimeText,
                 _ => throw new ArgumentException($"Wrong {nameof(DataType)}")
             };
 
+
+        private void UpdateEscapeButtonVisibility()
+        {
+            const int minCrimeToUse = 0;
+            const int maxCrimeToUse = 2;
+            const int minCrimeToShow = 0;
+            const int maxCrimeToShow = 5;
+
+            int crimeValue = _crime.Value;
+            bool canUse = minCrimeToUse <= crimeValue && crimeValue <= maxCrimeToUse;
+            bool canShow = minCrimeToShow <= crimeValue && crimeValue <= maxCrimeToShow;
+
+            _escapeButton.interactable = canUse;
+            _escapeButton.gameObject.SetActive(canShow);
+        }
 
         private void Fight()
         {
@@ -149,6 +186,14 @@ namespace BattleScripts
 
             string color = isVictory ? "#07FF00" : "#FF0000";
             string message = isVictory ? "Win" : "Lose";
+
+            Debug.Log($"<color={color}>{message}!!!</color>");
+        }
+
+        private void Escape()
+        {
+            string color = "#FFB202";
+            string message = "Escaped";
 
             Debug.Log($"<color={color}>{message}!!!</color>");
         }
